369505fd3a442ed64c4a87c484b9be8192847ba0
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-05 17:46:39 +0900

    [지수]

--- ACDCBoostCourse/ACDCBoostCourse/Week 1/1주차 과제_3 디버깅 툴 이용하기.cpp
+++ ACDCBoostCourse/ACDCBoostCourse/Week 1/1주차 과제_3 디버깅 툴 이용하기.cpp
@@ -1,10 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-	int a = 3;
-	cout << a << endl;
-	cout << "Hello World!\n";
+int main() {
+    double a = 10;
+
+    for (int i = 0; i < 99; i++) {
+
+        a /= 2;
+    }
+
+    cout << fixed;//실수부 자리제한
+    cout.precision(40);//소수점아래 40자리
+    cout << a << endl;
 
 }
---
+++ ACDCBoostCourse/Debug/ACDCBoostCourse.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int getSequenceNumber() {
+    int a = 10;
+
+    for (int i = 0; i < 99; i++) {
+        a /= 2;
+    }
+
+    return a;
+}
+
+int main() {
+
+    cout << getSequenceNumber() << endl;
+
+}
