7c460966352f3aa46ba24dea0dd759a8e9577beb
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-06 20:31:47 +0900

    [지수] 2주차 과제_2257

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 2/2257_숫자의개수.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num[10] = { 0 };
+	int a, b, c;
+	int mul;
+	int result;
+
+	cin >> a >> b >> c;
+	mul = a * b * c;
+
+	for (int i = 0; i < 10; i++) {
+		result = mul % 10;
+		num[result]++;
+		mul = mul/10;
+
+		if (mul == 0) {
+			break;
+		}
+	}
+	
+	for (int i = 0; i < 10; i++) { // 출력
+		cout << num[i] << endl;
+	}
+}
