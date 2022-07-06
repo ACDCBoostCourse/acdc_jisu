12bdf28833f308d36150f585a1cd1db516dc02eb
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-06 20:31:47 +0900

    [지수] 폴더링 수정

---
+++ ACDCBoostCourse/ACDCBoostCourse/Section 1_준비/Week 1/1주차 과제_3 디버깅 툴 이용하기.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 10;
+
+    for (int i = 0; i < 99; i++) {
+
+        a /= 2;
+    }
+
+    cout << fixed;//실수부 자리제한
+    cout.precision(40);//소수점아래 40자리
+    cout << a << endl;
+
+}
