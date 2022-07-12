6bd9bb4a93f4ed36b4bf9697a2a685362e6ca346
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-12 22:02:29 +0900

    [지수] 2주차 과제_1157

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 2/1157_단어공부.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string english;
+	int num[26] = { 0 };
+
+	cin >> english;
+
+	int length = english.length();
+	for (int i = 0; i < length; i++) {
+		if (english[i] < 97) { //대문자 65~90
+			num[english[i] - 65]++;
+		}
+		else{
+			num[english[i] - 97]++; //소문자 97~122
+		}
+	}
+
+	int alpha, max = 0;
+	for (int i = 0; i < 26; i++) {
+		if (max < num[i]) {
+			max = num[i];
+			alpha = i;
+		}
+	}
+
+	int over = 0 ; //중복 
+	for (int i = 0; i < 26; i++) {
+		if (max == num[i]){
+			over++;
+		}
+	}
+	//출력
+	if (over > 1) {
+		cout << "?";
+	}
+	else {
+		cout << (char)(alpha + 65);
+	}
+}
