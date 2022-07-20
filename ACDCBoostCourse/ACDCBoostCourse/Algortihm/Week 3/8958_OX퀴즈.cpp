391f7df8447507be02857f4ba356af9d2d72cc7c
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-20 09:48:42 +0900

    [지수] 3주차 과제_8958

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 3/8958_OX퀴즈.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	int num;
+	cin >> num;
+
+	string OX;
+	char input;
+
+	for (int i = 0; i < num; i++) {
+		cin >> OX;
+		int sum = 0;
+		int point = 0;
+		int length = OX.length();
+		
+		for (int j = 0; j < length; j++) {
+			input = OX[j];
+			if (input == 'O') {
+				++point;
+			}
+			else if (input == 'X') {
+				point = 0;
+			}
+			sum = sum + point;
+		}
+		cout << sum << endl;
+	}
+}
