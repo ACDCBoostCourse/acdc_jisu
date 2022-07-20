a60ac2e6ac23e4698cd6efd6c7e11404022273a6
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-07-20 11:56:30 +0900

    문제를 잘못 읽어 런타임 에러 발생..

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 3/1769_3의배수_.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// 0~9 -> 48~57
+	string input;
+	cin >> input;
+
+	int count = 0;
+
+	while (1) {
+		int length = input.length();
+		int sum = 0;
+		int num_input = stoi(input);
+
+		if (num_input < 10) {
+			if (num_input % 3 == 0) {
+				cout << count << endl << "YES";
+				break;
+			}
+			else {
+				cout << count << endl << "NO";
+				break;
+			}
+		}
+		else {
+			for (int i = 0; i < length; i++) { // 각 자리 합 구하기
+				int number = input[i] - 48;
+				sum = sum + number;
+			}
+			count++;
+			input = to_string(sum);
+			continue;
+		}
+	}
+	return 0;
+}
