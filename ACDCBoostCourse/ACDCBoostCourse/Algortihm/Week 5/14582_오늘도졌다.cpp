423d44255d205d9eae2ec9cfc7e6ee7f4849f002
전기정보공학과 강지수 <rkdwltndlsep@officestu.seoultech.ac.kr>
2022-08-02 13:06:47 +0900

    [지수] 5주차 과제_14582

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 5/14582_오늘도졌다.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int FirstTeam[9];
+	int SecondTeam[9];
+
+	for (int i = 0; i < 9; i++) { //울림 제미니스 점수
+		cin >> FirstTeam[i];
+	}
+
+	for (int i = 0; i < 9; i++) { //스타트링크 걸리버스 점수
+		cin >> SecondTeam[i];
+	}
+	int SumFirst = 0;
+	int SumSecond = 0;
+
+	for (int i = 0; i < 9; i++) {
+		SumFirst = SumFirst + FirstTeam[i];
+		if (SumFirst > SumSecond) {
+			cout << "Yes";
+			return 0;;
+		}
+		SumSecond = SumSecond + SecondTeam[i];
+	}
+	cout << "No";
+	return 0;
+}
